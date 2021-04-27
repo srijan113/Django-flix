4dce51086e74e2d4dd73be2f019962aa389ebaf2
srijan113 <srijan.pokhrel113@gmail.com>
2021-04-28 01:04:06 +0545

    proxy models done

--- videos/admin.py
+++ videos/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
+from .models import Video, AllVideoProxy, PublishedVideoProxy, LiveVideoProxy
 
-# Register your models here.
+
+class AllVideoProxyAdmin(admin.ModelAdmin):
+    list_display = ['title', 'live', 'status']
+    list_filter = ['status']
+    search_fields = ['video_id']
+    readonly_fields = ['id', 'publish_timestamp']
+
+    class Meta:
+        model = AllVideoProxy
+
+admin.site.register(AllVideoProxy, AllVideoProxyAdmin)
+
+
+class PublishedVideoAdmin(admin.ModelAdmin):
+    list_display = ['title', 'live', 'status']
+    list_filter = ['status']
+    search_fields = ['video_id']
+    readonly_fields = ['id', 'publish_timestamp']
+
+    class Meta:
+        model = PublishedVideoProxy
+
+    def get_queryset(self, request):
+        return PublishedVideoProxy.objects.filter(status=Video.VideoStatus.PUBLISH)
+
+
+admin.site.register(PublishedVideoProxy, PublishedVideoAdmin)
+
+
+
+class LiveVideoProxyAdmin(admin.ModelAdmin):
+    list_display = ['title', 'live', 'status']
+    list_filter = ['status']
+    search_fields = ['video_id']
+    readonly_fields = ['id', 'publish_timestamp']
+
+    class Meta:
+        model = LiveVideoProxy
+
+    def get_queryset(self, request):
+        return PublishedVideoProxy.objects.filter(live=True)
+
+
+admin.site.register(LiveVideoProxy, LiveVideoProxyAdmin)
--- videos/models.py
+++ videos/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Video(models.Model):
+    class VideoStatus(models.TextChoices):
+        PUBLISH = 'PU', 'Publish'
+        DRAFT = 'DU', 'Draft'
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    slug = models.SlugField(blank=True, null=True)
+    video_id = models.CharField(max_length=20, unique=True)
+    timestamp = models.DateTimeField(auto_now_add= True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2, choices=VideoStatus.choices, default=VideoStatus.DRAFT)
+    publish_timestamp = models.DateTimeField(auto_now=False, auto_now_add=False, blank=True, null=True)
+    live = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        if self.slug is None:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+
+
+class AllVideoProxy(Video):
+    class Meta:
+        proxy = True
+        verbose_name = 'All Video'
+        verbose_name_plural = "All Videos"
+
+
+class PublishedVideoProxy(Video):
+    class Meta:
+        proxy = True
+        verbose_name = 'Published Video'
+        verbose_name_plural = "Published Videos"
+
+
+class LiveVideoProxy(Video):
+    class Meta:
+        proxy = True
+        verbose_name = 'Live Video'
+        verbose_name_plural = 'Live Videos'
